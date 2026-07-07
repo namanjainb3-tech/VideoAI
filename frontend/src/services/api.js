9954abf36662d5b638631523684e45223112a4c3
Naman Jain <namanjainb3@gmail.com>
2026-07-07 10:09:07 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
 
-    baseURL: "https://2xlh3ih4236p7p-5000.proxy.runpod.net/",
+    baseURL: "https://v5p3oy0ywlgkbb-5000.proxy.runpod.net/",
 
 });
 
